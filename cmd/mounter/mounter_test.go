bcfe66f058fe4cf15f87e4f2f4cfdc6b8dbac593
akalenyu <51477153+akalenyu@users.noreply.github.com>
2022-01-31 17:21:32 +0200

    Drop some fields from lsblk output so we don't fail (#218)
    The boolean fields in newer versions (rm, ro) are outputted as true/false in --json:
    ```bash
    {
       "blockdevices": [
          {"name":"rbd0", "maj:min":"251:0", "rm":false, "size":"40G", "ro":false, "type":"disk", "mountpoint":"/host/var/hpvolumes/csi"}
       ]
    }
    bash-5.1# lsblk --version
    lsblk from util-linux 2.36.2
    ```
    In older lsblk versions this is not the case:
    ```bash
    {
       "blockdevices": [
          {"name": "vdb", "maj:min": "252:16", "rm": "0", "size": "120G", "ro": "0", "type": "disk", "mountpoint": null}
       ]
    }
    [root@hpp-pool-sno-test-infra-cluster-621da615-master-0-7d9ccbfctx8mn /]# lsblk --version
    lsblk from util-linux 2.32.1
    ```
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

---
+++ cmd/mounter/mounter_test.go
@@ -0,0 +1,52 @@
+/*
+Copyright 2021 The hostpath provisioner operator Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package main
+
+import (
+	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Mounter tests", func() {
+	Context("lsblk JSON parsing", func() {
+		intJSON := `{
+			"blockdevices": [
+			   {"name": "vdb", "maj:min": "252:16", "rm": "1", "size": "120G", "ro": "0", "type": "disk", "mountpoint": "/host/var/hpvolumes/csi"}
+			]
+		 }`
+		boolJSON := `{
+			"blockdevices": [
+			   {"name":"rbd0", "maj:min":"251:0", "rm":true, "size":"120G", "ro":false, "type":"disk", "mountpoint":"/host/var/hpvolumes/csi"}
+			]
+		 }`
+
+		table.DescribeTable("should not panic over json booleans with", func(jsonStr string) {
+			// Override lsblk cmd
+			lsblkCommand = func(source string) ([]byte, error) {
+				Expect(source).To(Equal("test"))
+				return []byte(jsonStr), nil
+			}
+
+			deviceInfos, err := lookupDeviceInfoByVolume("test")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(deviceInfos[0].Size).To(Equal("120G"))
+		},
+			table.Entry("integer value", intJSON),
+			table.Entry("actual boolean", boolJSON),
+		)
+	})
+})
