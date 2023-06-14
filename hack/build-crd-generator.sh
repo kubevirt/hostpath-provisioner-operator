0a00a82f2d35b0fc5811aa251010bcf263ba7f0e
Alexander Wels <awels@redhat.com>
2023-06-14 13:34:39 -0500

    Allow dynamic linking of binaries
    The release was creating statically linked binaries and there is no
    particular reason for this. Doing dynamically linked binaries
    would decrease the container size.
    
    Update deprecated registry references to the current ones
    
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- hack/build-crd-generator.sh
+++ hack/build-crd-generator.sh
@@ -17,4 +17,4 @@ script_dir="$(cd "$(dirname "$0")" && pwd -P)"
 source "${script_dir}"/common.sh
 ensureArmAvailable
 
-CGO_ENABLED=0 go build -a -ldflags '-extldflags "-static"' -o _out/crd-generator tools/crd-generator/crd-generator.go
+CGO_ENABLED=1 go build -a -tags strictfipsruntime -ldflags '-extldflags' -o _out/crd-generator tools/crd-generator/crd-generator.go
